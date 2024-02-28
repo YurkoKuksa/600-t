f3964688b9d8071dfc2b54e14ebac9edac7a716b
YurkoKuksa <145128983+YurkoKuksa@users.noreply.github.com>
2024-02-28 06:13:49 +0100

    contacts_page_realized

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,13 +1,98 @@
+import {
+  AddressWrapper,
+  CollageName,
+  ContactBox,
+  ContactLink,
+  ContentBox,
+  Description,
+  JobBox,
+  JobTitle,
+  ListStyled,
+  MainBox,
+  MainTitle,
+  SpanWrapper,
+  SvgItem,
+} from "./Contacts.styled";
+
+import location from "../../assets/svg/location.svg";
+import phone from "../../assets/svg/call.svg";
+import mail from "../../assets/svg/mail.svg";
 const Contacts = () => {
   return (
-    <div>
-      <p>Contacts</p>
-      <img
-        src="https://www.ikea.com/nl/en/images/products/kopparfall-picture-moonscape__0997459_pe822680_s5.jpg"
-        alt=""
-        width={650}
-      />
-    </div>
+    <MainBox>
+      <ContactBox>
+        <MainTitle>Контакти</MainTitle>
+        <ContentBox>
+          <Description>
+            Для отримання подальшої інформації, зворотнього зв'язку, можливостей
+            співпраці або подання пропозицій, будь ласка, звертайтеся за
+            контактними даними.
+          </Description>
+          <AddressWrapper>
+            <ul>
+              <ListStyled>
+                <ContactLink href="mailto:kuksasergei@ukr.net">
+                  <SpanWrapper>
+                    <SvgItem src={mail} alt="локація" width={20} /> &nbsp;
+                  </SpanWrapper>
+                  &nbsp; kuksasergey55@gmail.com
+                </ContactLink>
+              </ListStyled>
+
+              <ListStyled>
+                <ContactLink href="tel:+380968103053">
+                  <SpanWrapper>
+                    <SvgItem src={phone} alt="локація" width={22} /> &nbsp;
+                  </SpanWrapper>
+                  +38 (096) 810-30-53
+                </ContactLink>
+              </ListStyled>
+
+              <ListStyled>
+                <SpanWrapper>
+                  <SvgItem src={location} alt="локація" width={24} /> &nbsp;
+                </SpanWrapper>
+                <ContactLink
+                  href="https://www.google.com/maps/d/viewer?mid=1G6etACP-7wEXQcc5rTqkMA9uE-U&hl=en_US&ll=49.90016367377482%2C28.591063499999994&z=15"
+                  target="_blank"
+                >
+                  Місто Бердичів, Україна
+                </ContactLink>
+              </ListStyled>
+            </ul>
+          </AddressWrapper>
+          <JobBox>
+            <JobTitle> Місце роботи:</JobTitle>
+            <ul>
+              <CollageName>
+                <p>Бердичівський коледж промисловості, економіки та права</p>
+              </CollageName>
+
+              <ListStyled>
+                <ContactLink
+                  href="https://www.google.com/maps/d/viewer?mid=1G6etACP-7wEXQcc5rTqkMA9uE-U&hl=en_US&ll=49.8987250007059%2C28.57604945000001&z=20"
+                  target="_blank"
+                >
+                  вул. Молодогвардійська, 2а, Бердичів Україна
+                </ContactLink>
+              </ListStyled>
+              <ListStyled>
+                <ContactLink href="https://www.bfcpep.org.ua/" target="_blank">
+                  https://www.bfcpep.org.ua/
+                </ContactLink>
+              </ListStyled>
+            </ul>
+          </JobBox>
+        </ContentBox>
+      </ContactBox>
+    </MainBox>
   );
 };
 export default Contacts;
+
+//  <p>Contacts</p>
+//     <img
+//       src="https://www.ikea.com/nl/en/images/products/kopparfall-picture-moonscape__0997459_pe822680_s5.jpg"
+//       alt=""
+//       width={650}
+//     />
---
+++ src/pages/Contacts/Contacts.styled.js
@@ -0,0 +1,106 @@
+import styled from "styled-components";
+
+import tasks from "../../assets/images/chemistry91.png";
+
+export const MainBox = styled.div`
+  padding: 3px;
+  background-color: #c0c0c0;
+  max-width: 100%;
+  margin: 0;
+  border: 1px solid cornflowerblue;
+  flex-grow: 1;
+  box-shadow: 0px 1px 6px 0px rgba(46, 47, 66, 0.08),
+    0px 1px 1px 0px rgba(46, 47, 66, 0.16),
+    0px 2px 1px 0px rgba(46, 47, 66, 0.08);
+  display: flex;
+`;
+
+export const ContactBox = styled.div`
+  border-radius: 10px;
+  background-image: linear-gradient(
+      rgba(241, 241, 241, 0.4),
+      rgba(241, 241, 241, 0.4)
+    ),
+    url(${tasks});
+  background-repeat: no-repeat;
+  background-size: cover;
+  min-height: 300px;
+  flex-grow: 1;
+  padding: 40px 20px;
+  border: 1px solid cornflowerblue;
+
+  position: relative;
+`;
+
+export const MainTitle = styled.h1`
+  text-align: center;
+  margin-bottom: 30px;
+  /* margin-left: 120px; */
+  color: #808080;
+  /* width: 500px; */
+  justify-content: flex-end;
+  font-size: 40px;
+`;
+
+export const Description = styled.p`
+  font-size: 22px;
+  color: #808080;
+  padding: 0 50px;
+  text-indent: 50px;
+  line-height: 1.6;
+  margin-bottom: 20px;
+`;
+
+export const ContentBox = styled.div`
+  flex-direction: column;
+  align-items: flex-end;
+  display: flex;
+  gap: 20px;
+`;
+
+export const AddressWrapper = styled.address`
+  width: 500px;
+
+  justify-content: flex-end;
+  margin-bottom: 20px;
+`;
+
+export const ContactLink = styled.a`
+  cursor: pointer;
+`;
+
+export const ListStyled = styled.li`
+  margin-bottom: 10px;
+
+  font-size: 18px;
+
+  &:hover {
+    color: #dbd7d7;
+  }
+`;
+
+export const SpanWrapper = styled.span`
+  /* color: red; */
+`;
+
+export const SvgItem = styled.img`
+  display: inline-block;
+`;
+
+export const JobBox = styled.div`
+  width: 500px;
+  justify-content: flex-end;
+  align-items: center;
+`;
+
+export const JobTitle = styled.h2`
+  margin-bottom: 15px;
+  color: #808080;
+  font-size: 26px;
+`;
+
+export const CollageName = styled.p`
+  font-weight: 500;
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
